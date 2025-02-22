3edd6de1652e674d9c6ac2fdae537e2adb97bf96
shhady <shhadyse@gmail.com>
2025-02-22 21:21:53 +0200

    upload first

--- app/components/CartButton.jsx
+++ app/components/CartButton.jsx
@@ -15,7 +15,7 @@ export default function CartButton() {
     >
       <FiShoppingCart className="w-6 h-6" />
       {totalItems > 0 && (
-        <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+        <span className="absolute -top-1 -right-1 bg-blue-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
           {totalItems}
         </span>
       )}
